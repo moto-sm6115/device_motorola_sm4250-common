2b3fbd479a7ee8e2995e8d1d2f03fb2f2b476a22
rickyniu <rickyniu@google.com>
2021-07-29 22:03:38 -0500

    sm4250-common: Install gsi_keys into ramdisk, to boot a developer GSI with verified boot
    Signed-off-by: rickyniu <rickyniu@google.com>

--- common.mk
+++ common.mk
@@ -17,8 +17,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/motorola/sm4250-common/sm4250-common-vendor.mk)
